eb79a6c32cebbeb60bb8d276c5f31b7d7573a916
Jaime Inostroza <bladimirosky@gmail.com>
2020-09-05 15:20:07 -0400

    add base drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,42 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
+      drawer: _drawer(),
       body: Center(child: Text('This is home page')),
     );
   }
+}
+
+Widget _drawer() {
+  return Drawer(
+    child: ListView(
+      padding: EdgeInsets.zero,
+      children: const <Widget>[
+        DrawerHeader(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Text(
+            'Drawer Header',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.message),
+          title: Text('Messages'),
+        ),
+        ListTile(
+          leading: Icon(Icons.account_circle),
+          title: Text('Profile'),
+        ),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text('Settings'),
+        ),
+      ],
+    ),
+  );
 }
