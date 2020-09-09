e9bc54c7fe4eff2d3aec586e75f0d0999f11a10d
Jaime Inostroza <bladimirosky@gmail.com>
2020-09-08 23:34:53 -0300

    listing post with streambuilder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -25,16 +26,37 @@ class _HomePageState extends State<HomePage> {
       //       ? Text('User logged successfully')
       //       : Text('No user logged in'),
       // ),
-      body: (Container()),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (isAuthenticated) {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => PostNewPage()));
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collectionGroup('posts').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Text('Loading...');
+            default:
+              return ListView(
+                children: snapshot.data.docs.map((DocumentSnapshot document) {
+                  return ListTile(
+                    title: Text(
+                      document.data()['title'],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(document.data()['content']),
+                  );
+                }).toList(),
+              );
           }
         },
-        child: Icon(Icons.note_add),
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     if (isAuthenticated) {
+      //       Navigator.push(context,
+      //           MaterialPageRoute(builder: (context) => PostNewPage()));
+      //     }
+      //   },
+      //   child: Icon(Icons.note_add),
+      // ),
     );
   }
 }
